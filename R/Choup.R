f936c883e46da6b429f8bfcf0a128ee6d5e0ee56
Bruciamacchie <max.bruciamacchie@gmail.com>
2023-06-20 17:19:57 +0200

    Init

---
+++ R/Choup.R
@@ -0,0 +1,15 @@
+#' Coefficients de houppier.
+#'
+#' @description Coefficients de houppier issus de la base de données de l'AFI. Ils permettent de calculer le couvert
+#' à partir de la surface terrière par essence et classe de diamètre.
+#'
+#' @format data frame contenant près de 425 lignes et 3 variables
+#'
+#' @examples
+#' data(Choup)
+#'
+#'
+"Choup"
+
+
+# usethis::use_data(Choup, overwrite = T)
